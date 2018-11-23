887410f41e1f73cce0fb88351bf5bef5047dfb36
Adam Eggleston <adam.eggleston@auspost.com.au>
2018-11-23 22:14:30 +1100

    added ghpages

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, Injectable } from '@angular/core';
+// import { SwUpdate } from '@angular/service-worker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
+
+@Injectable()
+export class LogUpdateService {
+
+  // constructor(updates: SwUpdate) {
+  //   updates.available.subscribe(event => {
+  //     console.log('current version is', event.current);
+  //     console.log('available version is', event.available);
+  //   });
+  //   updates.activated.subscribe(event => {
+  //     console.log('old version was', event.previous);
+  //     console.log('new version is', event.current);
+  //   });
+  // }
+}
