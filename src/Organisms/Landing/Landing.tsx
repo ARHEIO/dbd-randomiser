65c11d367d7470ee3888ccff669becbc06b09ea9
Adam Eggleston <adam.rh.eggleston@gmail.com>
2020-05-17 14:51:19 +1000

    Workflow

--- src/Organisms/Landing/Landing.tsx
+++ src/Organisms/Landing/Landing.tsx
@@ -6,12 +6,12 @@ function Landing() {
   return (
     <div className="landing-container">
        <Link className="link-button" to={`/survivor`}>
-          <img alt="survivor" src="https://gamepedia.cursec dn.com/deadbydaylight_gamepedia_en/b/b3/IconHelpLoading_survivor.png?version=5b3fe327496a6c81537c17c036e9745f" />
+          <img alt="survivor" src="https://dqr3pglopijar.cloudfront.net/assets/iconHelpLoading_survivor.png" />
           <p>Randomise survivor build</p>
         </Link>
 
         <Link className="link-button" to={`/killer`}>
-          <img alt="killer" src="https://gamepedia.curse cdn.com/deadbydaylight_gamepedia_en/0/06/IconHelpLoading_killer.png?version=317ddad24cfb2b43f330e613179a257a" />
+          <img alt="killer" src="https://dqr3pglopijar.cloudfront.net/assets/iconHelpLoading_killer.png" />
           <p>Randomise killer build</p>
         </Link>
     </div>
